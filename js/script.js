9f9d48f0d046d92abf332e73b1056b9193c0adeb
Your Name <you@example.com>
2021-04-07 16:48:39 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#table-statistic').DataTable({
+        "language": {
+            "url": "plug-ins/1.10.24/russian.json"
+        }
+    }); 
+    $('#available-rooms').DataTable({
+        "language": {
+            "url": "plug-ins/1.10.24/russian.json"
+        }
+    }); 
+} );
+
