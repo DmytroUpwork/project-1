600ac025991d144502bf3ba2ea7b5ed5a9d0b7e4
Your Name <you@example.com>
2021-04-07 17:27:21 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,37 @@
 $(document).ready(function() {
+    $('#table-service').DataTable({
+        "language": {
+            "url": "plug-ins/1.10.24/russian.json"
+        },
+        "paging": false,
+        "info": false,
+        "sDom": ''
+    }); 
+
+    $('#table-list').DataTable({
+        "language": {
+            "url": "plug-ins/1.10.24/russian.json"
+        },
+        "paging": false,
+        "info": false,
+        "sDom": ''
+    }); 
+
+    $('#table-popular').DataTable({
+        "language": {
+            "url": "plug-ins/1.10.24/russian.json"
+        },
+        "paging": false,
+        "info": false,
+        "sDom": ''
+    }); 
+
     $('#table-statistic').DataTable({
         "language": {
             "url": "plug-ins/1.10.24/russian.json"
         }
     }); 
+
     $('#available-rooms').DataTable({
         "language": {
             "url": "plug-ins/1.10.24/russian.json"
