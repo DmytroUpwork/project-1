5512e213b40210f0193b3df002bdf5382da14daa
Your Name <you@example.com>
2021-04-08 11:06:31 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,26 +1,17 @@
 $(document).ready(function() {
     $('#table-service').DataTable({
-        "language": {
-            "url": "plug-ins/1.10.24/russian.json"
-        },
         "paging": false,
         "info": false,
         "sDom": ''
     }); 
 
     $('#table-list').DataTable({
-        "language": {
-            "url": "plug-ins/1.10.24/russian.json"
-        },
         "paging": false,
         "info": false,
         "sDom": ''
     }); 
 
     $('#table-popular').DataTable({
-        "language": {
-            "url": "plug-ins/1.10.24/russian.json"
-        },
         "paging": false,
         "info": false,
         "sDom": ''
