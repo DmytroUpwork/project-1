d762717a167f0c6fafd665ad0f9d2adbc95afce0
Your Name <you@example.com>
2021-04-14 21:11:13 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     // Таблицы левого блока 
     $('#table-service').DataTable({
         "language": {
-            "url": "../plug-ins/1.10.24/russian.json"
+            "url": "~/plug-ins/1.10.24/russian.json"
         },
         "scrollY":        "410px",
         "scrollCollapse": true,
@@ -13,7 +13,7 @@ $(document).ready(function() {
 
     $('#table-list').DataTable({
         "language": {
-            "url": "../plug-ins/1.10.24/russian.json"
+            "url": "~/plug-ins/1.10.24/russian.json"
         },
         "scrollY":        "410px",
         "scrollCollapse": true,
@@ -22,7 +22,7 @@ $(document).ready(function() {
 
     $('#table-popular').DataTable({
         "language": {
-            "url": "../plug-ins/1.10.24/russian.json"
+            "url": "~/plug-ins/1.10.24/russian.json"
         },
         "scrollY":        "410px",
         "scrollCollapse": true,
@@ -33,13 +33,13 @@ $(document).ready(function() {
     // Таблицы правого блока
     $('#table-statistic').DataTable({
         "language": {
-            "url": "../plug-ins/1.10.24/russian.json"
+            "url": "~/plug-ins/1.10.24/russian.json"
         }
     }); 
 
     $('#available-rooms').DataTable({
         "language": {
-            "url": "../plug-ins/1.10.24/russian.json"
+            "url": "~/plug-ins/1.10.24/russian.json"
         }
     }); 
 
