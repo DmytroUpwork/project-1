3c1b18e9be52562d073a07f51b1a98b49f9b1f8b
Your Name <you@example.com>
2021-04-17 08:50:52 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -49,13 +49,7 @@ function go_to_page(page_num){
 };
 
 
-
-
-
-
-
-
-
+// Скрипт таблицы DataTable 
 $(document).ready(function() {
 
   // Таблицы левого блока 
@@ -86,124 +80,104 @@ $(document).ready(function() {
       "paging": false,
   }); 
 
-
-  // Таблицы правого блока
-  $('#table-statistic').DataTable({
-      "language": {
-          "url": "/project-1/plug-ins/1.10.24/russian.json"
-      }
-  }); 
-
-  $('#available-rooms').DataTable({
-      "language": {
-          "url": "/project-1/plug-ins/1.10.24/russian.json"
-      }
-  }); 
-
 });
 
 
 
+var show_ar_form = false;
 
-
-
-// document.addEventListener('DOMContentLoaded', function(){ 
-
-
-  var show_ar_form = false;
-
-  function change_reviewtype(num) {
-    var tsiddesc = document.getElementById('tsiddesc');
-    if (num == 2) {
-      tsiddesc.style.display = document.all && !document.querySelector ? 'block' : 'table-row';//ie7fix
-    } else {
-      tsiddesc.style.display = 'none';
-      //tsidposdesc.style.display = document.all && !document.querySelector ? 'block' : 'table-row';//ie7fix
-    }
-  };
-
-  // Скрипт добавления формы отправки отзыва 
-  function switch_arform(obj, position, is_start) {
-    if (show_ar_form) {
-      if (is_start) obj.style.position = 'static';
-
-      position += 130;
-      if (position < ar_form_height) {
-        obj.style.height = position + 'px';
-      } else {
-        position = ar_form_height;
-        obj.style.height = 'auto';
-        return;
-      }
-    } else {
-      position -= 130;
-      if (position > 0) {
-        obj.style.height = position + 'px';
-      } else {
-        obj.style.position = 'absolute';
-        obj.style.left = '-9999px';
-        obj.style.height = 'auto';
-        document.getElementById('ar_bullet').className = 'review_addicon bulletdown';
-        return;
-      }
-    }
-    setTimeout(function () {
-      switch_arform(obj, position, false);
-    }, 10);
+// Извещение при выборе "Претензия" в форме подачи отзыва 
+function change_reviewtype(num) {
+  var tsiddesc = document.getElementById('tsiddesc');
+  if (num == 2) {
+    tsiddesc.style.display = document.all && !document.querySelector ? 'block' : 'table-row';//ie7fix
+  } else {
+    tsiddesc.style.display = 'none';
+    //tsidposdesc.style.display = document.all && !document.querySelector ? 'block' : 'table-row';//ie7fix
   }
+};
 
-  function show_addreviewform(whatis) {
-    var new_show_ar_form;
-    if (whatis == 1) {
-      new_show_ar_form = true;
-    } else if (whatis == 2) {
-      new_show_ar_form = false;
+// Скрипт добавления формы отправки отзыва 
+function switch_arform(obj, position, is_start) {
+  if (show_ar_form) {
+    if (is_start) obj.style.position = 'static';
+
+    position += 130;
+    if (position < ar_form_height) {
+      obj.style.height = position + 'px';
     } else {
-      new_show_ar_form = !show_ar_form;
-    }
-    if (show_ar_form == new_show_ar_form) return;
-    show_ar_form = new_show_ar_form;
-
-    var review_form = document.getElementById('review_form');
-    ar_form_height = review_form.clientHeight;
-    if (show_ar_form) {
-      switch_arform(review_form, 0, true);
-      document.getElementById('ar_bullet').className = 'review_addicon bulletup';
-    } else {
-      switch_arform(review_form, ar_form_height, true);
-    }
-  }
-
-  // Скрипт рткрытия окна подтверждения удаления отзыва
-  function removeElem(delElem, attribute, attributeName) {
-    if (!(delElem && attribute && attributeName)) return;
-    return function(e) {
-      let target = e.target;
-      if (!(target.hasAttribute(attribute) ?
-          (target.getAttribute(attribute) === attributeName ? true : false) : false)) return;
-      let elem = target;
-      while (target != this) {
-        if (target.classList.contains(delElem)) {
-          let removeReviews = confirm("Вы действительно хотите удалить отзыв?");
-          if (removeReviews == true) {
-            target.remove();
-            return;
-          }
-        }
-        target = target.parentNode;
-      }
+      position = ar_form_height;
+      obj.style.height = 'auto';
       return;
-    };
+    }
+  } else {
+    position -= 130;
+    if (position > 0) {
+      obj.style.height = position + 'px';
+    } else {
+      obj.style.position = 'absolute';
+      obj.style.left = '-9999px';
+      obj.style.height = 'auto';
+      document.getElementById('ar_bullet').className = 'review_addicon bulletdown';
+      return;
+    }
   }
-  document.addEventListener("click", removeElem("review_block", "data-del", "delete"));
+  setTimeout(function () {
+    switch_arform(obj, position, false);
+  }, 10);
+}
 
-  // Скрипт отображения textarea поля для написания комментария 
-  document.onclick = function(event) { 
-    var target = event.target;  
-    var id = target.getAttribute('data-addComment');
-    if (!id) return;
-    var elem = document.getElementById(id);
-    elem.hidden = !elem.hidden;
+function show_addreviewform(whatis) {
+  var new_show_ar_form;
+  if (whatis == 1) {
+    new_show_ar_form = true;
+  } else if (whatis == 2) {
+    new_show_ar_form = false;
+  } else {
+    new_show_ar_form = !show_ar_form;
+  }
+  if (show_ar_form == new_show_ar_form) return;
+  show_ar_form = new_show_ar_form;
+
+  var review_form = document.getElementById('review_form');
+  ar_form_height = review_form.clientHeight;
+  if (show_ar_form) {
+    switch_arform(review_form, 0, true);
+    document.getElementById('ar_bullet').className = 'review_addicon bulletup';
+  } else {
+    switch_arform(review_form, ar_form_height, true);
+  }
+}
+
+// Скрипт открытия окна подтверждения удаления отзыва
+function removeElem(delElem, attribute, attributeName) {
+  if (!(delElem && attribute && attributeName)) return;
+  return function(e) {
+    let target = e.target;
+    if (!(target.hasAttribute(attribute) ?
+        (target.getAttribute(attribute) === attributeName ? true : false) : false)) return;
+    let elem = target;
+    while (target != this) {
+      if (target.classList.contains(delElem)) {
+        let removeReviews = confirm("Вы действительно хотите удалить отзыв?");
+        if (removeReviews == true) {
+          target.remove();
+          return;
+        }
+      }
+      target = target.parentNode;
+    }
+    return;
   };
+}
+document.addEventListener("click", removeElem("review_block", "data-del", "delete"));
 
-// });
+// Скрипт отображения textarea поля для написания комментария 
+document.onclick = function(event) { 
+  var target = event.target;  
+  var id = target.getAttribute('data-addComment');
+  if (!id) return;
+  var elem = document.getElementById(id);
+  elem.hidden = !elem.hidden;
+};
+
